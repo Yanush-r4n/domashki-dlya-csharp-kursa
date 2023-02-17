4c64e81e95a03aa6d80bed7ee63bcabc86c4dc82
Yanush-r4n <yana.mur.04@gmail.com>
2023-02-18 01:36:28 +0300

    task 13 solution

---
+++ seminar2/task13/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (номер цифры считается от левого края)
+
+Console.Clear();
+
+Console.Write("Введи чило, а я покажу его третью цифру, начиная отсчёт с левого края - ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 999)
+{
+    num = num%1000/100;
+    Console.WriteLine($"{num} твоя третья цифра");
+}
+else if (num > 99 && num < 1000)
+{
+    num = num/100;
+    Console.WriteLine($"{num} твоя третья цифра");
+}
+else if (num < -99 && num > -1000)
+{
+    num = num/100*(-1);
+    Console.WriteLine($"{num} твоя третья цифра");
+}
+else if (num < -999)
+{
+    num = num%1000/100*(-1);
+    Console.WriteLine($"{num} твоя третья цифра");
+}
+else
+{
+    Console.WriteLine($"У твоего числа {num} нет третьего числа так-то");
+}
