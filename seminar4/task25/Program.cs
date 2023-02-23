13d49b9ddd28bed66596de54ec546de0a556704b
Yanush-r4n <yana.mur.04@gmail.com>
2023-02-23 23:21:54 +0300

    task 25 solution

---
+++ seminar4/task25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+int NumberInput(string MessageToUser)
+{
+    Console.Write(MessageToUser);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ExpoundTheNumber(int number, int power)
+{
+    int result = 1;
+
+    for (int i = 1; i <= power; i++)
+    {
+        result = result * number;
+    }
+
+    return result;
+}
+
+int number = NumberInput("Введи число, которое я буду возводить в степень: ");
+int power = NumberInput("Введи саму степень: ");
+
+Console.Write($"Получается, если {number} возвести в {power}, получится {ExpoundTheNumber(number, power)}");
