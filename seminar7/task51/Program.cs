810413282ec943dc38d527883d1ebd7ccb193fb7
Yanush-r4n <yana.mur.04@gmail.com>
2023-03-10 16:14:52 +0300

    task 51 solution

---
+++ seminar7/task51/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Clear();
+
+int[,] Get2DArray(int raws, int columns)
+{
+    int[,] array = new int[raws, columns];
+
+    for (int i = 0; i < raws; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Random.Shared.Next(0, 10);
+        }
+    }
+
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) {Console.Write($"{array[i, j]} ");}
+        Console.WriteLine();
+    }
+}
+
+int DiagonalSum(int[,] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.GetLength(0) && i < array.GetLength(1); i++)
+    {
+        sum = sum + array[i, i];
+    }
+
+    return sum;
+}
+
+int raws = 4;
+int columns = 4;
+
+int[,] matrix = Get2DArray(raws, columns);
+
+Print2DArray(matrix);
+Console.WriteLine();
+Console.Write(DiagonalSum(matrix));
