2598693e41d6053a774e5b8ea26bf8d344332acd
Yanush-r4n <yana.mur.04@gmail.com>
2023-03-17 15:37:33 +0300

    task 69 solved

---
+++ seminar9/task69/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.Clear();
+
+int number = 6,
+    pow = 8;
+
+int NumInPow(int number, int power)
+{
+    if (power == 1) {return number;}
+
+    int result = number;
+    result *= NumInPow(number, power - 1);
+
+    return result;
+}
+
+Console.Write(NumInPow(number, pow));
