5458ca465db5be70bc1336babeff768fb01f3a0a
Yanush-r4n <yana.mur.04@gmail.com>
2023-03-17 15:36:48 +0300

    task 67 solved

---
+++ seminar9/task67/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+int number = 937;
+
+int SumOfDigits(int number)
+{
+    int sum = number % 10;
+    if (number > 10) {sum += SumOfDigits(number / 10);}
+    return sum;
+}
+
+Console.Write(SumOfDigits(number));
