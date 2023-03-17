ced0c9a5d2a7dbcbbc3f439edb3e4e098527245e
Yanush-r4n <yana.mur.04@gmail.com>
2023-03-17 15:35:39 +0300

    task 63 solved

---
+++ seminar9/task63/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+int number = 6;
+
+int Sequence(int n)
+{
+    if (n == 1) {return 1;}
+    Console.Write($"{Sequence(n - 1)}, ");
+    return n;
+}
+
+Console.Write(Sequence(number));
