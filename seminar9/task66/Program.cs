f67fdd77eb3093a39ee4841482b4a5373a7df9aa
Yanush-r4n <yana.mur.04@gmail.com>
2023-03-17 15:36:26 +0300

    task 66 solved

---
+++ seminar9/task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+int numOne = 1,
+    numTwo = 15;
+
+int SumFromMToN(int m, int n)
+{
+    if (m == n) return m;
+    int sum = m;
+    sum += SumFromMToN(m + 1, n);
+    return sum;
+}
+
+Console.Write(SumFromMToN(numOne, numTwo));
