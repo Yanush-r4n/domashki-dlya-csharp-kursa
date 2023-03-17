886aac879973f4e7f8a3551f27e00be41217da66
Yanush-r4n <yana.mur.04@gmail.com>
2023-03-17 15:36:09 +0300

    task 64 solved

---
+++ seminar9/task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// адайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+
+int number = 4;
+
+int FromNumTo1(int number)
+{
+    Console.Write($"{number}, ");
+    if (number == 2) return number;
+    FromNumTo1(number - 1);
+
+    return 1;
+}
+
+Console.WriteLine(FromNumTo1(number));
